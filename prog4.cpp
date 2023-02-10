11733e44c3e1bc744492faf413cefe55cdce4db4
random0901 <108359455+random0901@users.noreply.github.com>
2023-02-10 11:10:45 +0530

    Update prog4.cpp

--- prog4.cpp
+++ prog4.cpp
@@ -1,29 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include<omp.h>
-int fib(int n)
-{
-	if (n < 2) return n;
-	else return fib(n - 1) + fib(n - 2);
-}
-int main()
-{
-	int fibnumber[100], i, j, n;
-	printf("please enter the series limit\n");
+int main() {
+	int n, i;
+	printf("Number of terms : ");
 	scanf_s("%d", &n);
-#pragma omp parallel num_threads(2)
+	int* a = (int*)malloc(n * sizeof(int));
+	a[0] = 0;
+	a[1] = 1;
+	time_t st, et;
+	st = clock();
+	omp_set_num_threads(2);
+#pragma omp parallel
 	{
-		if (omp_get_thread_num() == 0)
+#pragma omp single
 		{
-			printf("there are %d threads\n", omp_get_num_threads());
-			printf("Threads %d generating numbers..\n", omp_get_thread_num());
-			for (i = 0; i < n; i++)
-				fibnumber[i] = fib(i);
+			printf("id of thread involved in the computation of fibonacci numbers = %d\n", omp_get_thread_num());
+			for (i = 2; i < n; i++)
+				a[i] = a[i - 2] + a[i - 1];
 		}
-		if (omp_get_thread_num() == 1) {
-			printf("Threads %d Printing numbers..\n", omp_get_thread_num());
-
-			for (j = 0; j < n; j++)
-				printf("%d\t", fibnumber[j]);
+#pragma omp single
+		{
+			printf("id of thread involved in the displaying of fibonacci numbers = %d\n", omp_get_thread_num());
+			printf("Fibonacci numbers : ");
+			for (i = 0; i < n; i++)
+				printf("%d ", a[i]);
+			printf("\n");
 		}
 	}
+	et = clock();
+	printf("Time Taken : %lfms\n", ((double)(et - st) * 1000 / CLOCKS_PER_SEC));
+	return 0;
 }
